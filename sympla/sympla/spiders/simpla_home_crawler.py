20045d324c14fc9f024fb66739b23fcb46f3e622
Raul Martins <raulmartinsj@outlook.com>
2017-01-15 10:00:03 -0200

    wip

--- sympla/sympla/spiders/simpla_home_crawler.py
+++ sympla/sympla/spiders/simpla_home_crawler.py
@@ -7,26 +7,21 @@ import random
 class SymplaGenrateUrlsSpider(scrapy.Spider):
     name = "sympla_generate_url"
 
-    def __init__(self):
-        pass
+    def __init__(self, complement=None, *args, **kwargs):
+        super(SymplaGenrateUrlsSpider, self).__init__(*args, **kwargs)
+        self.complement = complement
 
     def start_requests(self):
-        # url = 'http://www.sympla.com.br/eventos/belo-horizonte-mg'  # passo 1
-        # yield scrapy.Request(url=url, callback=self.parse)  # passo 1
+        if not self.complement:
+            url = 'http://www.sympla.com.br/eventos/sao-paulo-sp'
+        else:
+            url = 'http://www.sympla.com.br/eventos/{}'.format(self.complement)
 
-        #################################################################
-        urls = self.read_json_archive()
-        for url in urls:
-            for key in url.keys():
-                yield scrapy.Request(url=url[key], callback=self.parse)
-        #################################################################
+        yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
-        # for url in self.get_url_events_for_pages(response):  # passo 1
-        #     yield scrapy.Request(url=url, callback=self.parse_2)  # passo 1
-        #################################################################
-        yield self.save_data_from_event(response)
-        ################################################################
+        for url in self.get_url_events_for_pages(response):
+            yield scrapy.Request(url=url, callback=self.parse_2)
 
     def parse_2(self, response):
         yield self.get_data_url_events(response)
@@ -47,37 +42,6 @@ class SymplaGenrateUrlsSpider(scrapy.Spider):
             f.write(response.body)
         self.log('Saved file %s' % filename)
 
-    def read_json_archive(self, file_path=None):
-        if not file_path:
-            file_path = '/home/raul/projects/scrapy/crawler_sympla/sympla/bh.json'
-        _json = open(file_path, 'r')
-
-        data = json.load(_json)
-        return data
-
-    def save_data_from_event(self, response):
-        event = {}
-        payment = response.css('form span::text').extract()
-
-        event.update(
-            {'event_name':
-             response.css('title::text').extract()[0].replace("Sympla", "").replace("-", "")}
-        )
-        event.update({'event_date': response.css('div.col-md-12 div::text').extract()[2]})
-        event.update(
-            {'event_local': response.css('p.margin-bottom-20-xs::text').extract()[1]}
-        )
-        event.update(
-            {'event_description': response.css(
-                'div.container.border-top.border-left.border-right.event-section p'
-            ).extract()}
-        )
-        event.update({'event_productor': response.css('h4.kill-top-margin::text').extract()[2]})
-        event.update({'other_informations_productor': response.css('p.text-center-xs.text-left-md::text').extract()})
-        event.update({'link': response.url})
-        event.update({'payment': payment})
-        return event
-
     def get_numbers_of_events(self, response):
         list_of_number = response.css(
             'div.header-list div.container h3 strong::text'
@@ -90,7 +54,7 @@ class SymplaGenrateUrlsSpider(scrapy.Spider):
 
         for nan in range(0, self.get_numbers_of_events(response)):
             lista_events.append(
-                'http://www.sympla.com.br/eventos/sao-paulo-sp?ordem=relev%C3%A2ncia&pagina={}'.format(nan)
+                'http://www.sympla.com.br/eventos/{}?ordem=relev%C3%A2ncia&pagina={}'.format(self.complement, nan)
             )
         return lista_events
 
@@ -98,8 +62,9 @@ class SymplaGenrateUrlsSpider(scrapy.Spider):
 class SymplaCatchDataSpider(scrapy.Spider):
     name = "sympla_catch_data"
 
-    def __init__(self):
-        pass
+    def __init__(self, file, *args, **kwargs):
+        super(SymplaCatchDataSpider, self).__init__(*args, **kwargs)
+        self.file = file
 
     def start_requests(self):
         urls = self.read_json_archive()
@@ -112,7 +77,7 @@ class SymplaCatchDataSpider(scrapy.Spider):
 
     def read_json_archive(self, file_path=None):
         if not file_path:
-            file_path = '/home/raul/projects/scrapy/crawler_sympla/sympla/bh.json'
+            file_path = '/home/raul/projects/scrapy/crawler_sympla/sympla/sympla/{}.json'.format(self.file)
         _json = open(file_path, 'r')
 
         data = json.load(_json)
